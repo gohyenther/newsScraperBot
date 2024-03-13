7364dd29d95e3e8507f41d17f6e13658b31620bc
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2024-03-13 17:38:10 +0800

    news source exclusions

--- newsScraperBot.py
+++ newsScraperBot.py
@@ -5,7 +5,8 @@ from random import randint
 from bs4 import BeautifulSoup
 
 NUM_ARTICLES = 3 # Top n headlines
-
+EXCLUSIONS = ["Simply Wall St", "Simply Wall Street", "www.tradingview.com", "Trade Brains", "MarketWatch"] # Exclude news from these sources
+TITLE_EXCLUSIONS = ["Dividend", "Stock"]
 
 def scrape_news(company, category):
     time.sleep(randint(0,5)) # relax and don't spam google
@@ -21,21 +22,38 @@ def scrape_news(company, category):
     print(company + " - " + str(response.status_code))
 
     for el in soup.select("div.SoaBEf"):
+        source = el.select_one(".NUnG9d span").get_text()
         date = el.select_one(".LfVVr").get_text()
-        if "hour" in date or "day" in date or "minute" in date or "week ago" in date:
+        title = el.select_one("div.MBeuO").get_text()
+        snippet = el.select_one(".GI74Re").get_text()
+        link = el.find("a")["href"]
+
+        exclude_flag = False
+
+        # news source exclusions:
+        if source in EXCLUSIONS: exclude_flag = True
+
+        # news title exclusions:
+        for title_exclude in TITLE_EXCLUSIONS:
+            if title_exclude in title:
+                exclude_flag = True
+                break
+        
+        # only include the past 7 days of latest news
+        if not exclude_flag and ("hour" in date or "day" in date or "minute" in date or "week ago" in date):
             if category == 1:
                 news_results["Contractor"].append(company)
             elif category == 2:
                 news_results["Topic"].append(company)
-            news_results["Title"].append(el.select_one("div.MBeuO").get_text())
-            news_results["Snippet"].append(el.select_one(".GI74Re").get_text())
+            news_results["Title"].append(title)
+            news_results["Snippet"].append(snippet)
             news_results["Date"].append(date)
-            news_results["Source"].append(el.select_one(".NUnG9d span").get_text())
-            news_results["Link"].append(el.find("a")["href"])
+            news_results["Source"].append(source)
+            news_results["Link"].append(link)
+
 
 # Scrape Contractors News
 news_results = {"Contractor":[], "Title":[], "Snippet":[], "Date":[], "Source":[], "Link":[]}
-
 watchlist = pd.read_csv("watchlist.csv")['Companies']
 for company in watchlist:
     scrape_news(company, 1) # category: 1
@@ -45,7 +63,6 @@ news_results.to_csv("news_contractors.csv", index=False)
 
 # Scrape Topics News
 news_results = {"Topic":[], "Title":[], "Snippet":[], "Date":[], "Source":[], "Link":[]}
-
 topics = pd.read_csv("topics.csv")['Topics']
 for topic in topics:
     scrape_news(topic, 2) # category: 2
